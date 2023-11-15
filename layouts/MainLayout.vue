167075b1b2aada13828cd0d3a4228d020a54b5b1
plantnt <godzillagmk2204@gmail.com>
2023-11-15 14:35:44 -0500

    new file:   .gitignore 	new file:   README.md 	new file:   app.vue 	new file:   layouts/MainLayout.vue 	new file:   nuxt.config.ts 	new file:   package-lock.json 	new file:   package.json 	new file:   pages/index.vue 	new file:   public/favicon.ico 	new file:   server/tsconfig.json 	new file:   tsconfig.json

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="MainLayout" class="w-full fixed z-50">
+        <div id="TopMenu" class="w-full bg-[#FAFAFA] border-b md:block hidden">
+            <ul class=
+            "flex 
+            items-center 
+            justify-end 
+            text-xs 
+            text-[#333] 
+            font-light 
+            px-2 
+            h-10 
+            bg-[#FAFAFA] 
+            max-w-[1200px]"
+            >
+                <li class="border-r border-r-gray-400 px-3 hover:text-[#7841fa] cursor-pointer">
+                    Vende en Furnimate
+                </li>
+                <li class="border-r border-r-gray-400 px-3 hover:text-[#7841fa] cursor-pointer">
+                    Acerca de nosotros
+                </li>
+                <li class="border-r border-r-gray-400 px-3 hover:text-[#7841fa] cursor-pointer">
+                    Terminos y Condiciones
+                </li>
+                <li class="border-r border-r-gray-400 px-3 hover:text-[#7841fa] cursor-pointer">
+                    Ayuda
+                </li>
+                <li class="px-3 hover:text-[#7841fa] cursor-pointer">
+                    <Icon name="material-symbols:app-promo-sharp" size="17" />
+                    App
+                </li>
+                <li
+                    @mouseenter="isAccountMenu = true"
+                    @mouseleave="isAccountMenu = false"
+                    class="relative flex items-center px-2.5 hover:text-[#7841fa] h-full cursor-pointer"
+                    :class="isAccountMenu ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]':'border border-[#FAFAFA]'">
+                    <Icon name="material-symbols:account-circle" size="17"/>
+                    Account
+                    <Icon name="material-symbols:keyboard-arrow-down-rounded" size="15" class="ml-5"/>
+                </li>
+        </ul>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    let isAccountMenu = ref(false)
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <MainLayout>
+        <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+        </div>
+    </MainLayout>
+</template>
+
+<script setup lang="ts">
+import MainLayout from "~/layouts/MainLayout.vue";
+</script>
