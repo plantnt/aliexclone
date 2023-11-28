f70acce6cdca418a9fd5bbddae61599db57ed231
plantnt <godzillagmk2204@gmail.com>
2023-11-28 08:09:55 -0500

    new file:   components/ProductComponent.vue 	modified:   pages/index.vue

---
+++ components/ProductComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div
+    :id="`ProductComponent${product.id}`" 
+    class="bg-white inline-block rounded hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.15)] transition ease-all cursor-pointer">
+        <NuxtLink :to="`/item/${product.id}`">
+            <img class="rounded-t" :src="product.url">
+            <div id="ProductDetails">
+                <span class="flex items-center justify-start gap-3 px-1 pt-1">
+                    <span class="text-black text-xl font-semibold">${{ priceComputed }}</span>
+                    <span class="text-gray-500 text-sm text-light line-through">${{ oldPriceComputed }}</span>
+                </span>
+                <span class="px-1 relative -top-1.5 text-[#7841fa] text-xs font-semibold">
+                    Descuento de 5%
+                </span>
+
+                <div class="flex item-center gap-1 px-1 relative -top-1">
+                    <span class="bg-[#7841fa] text-while text-[9px] font-semibold px-1.5 rounded-sm"></span>
+                </div>
+            </div>
+        </NuxtLink>
+
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['product'])
+const {product} = toRefs(props)
+
+const priceComputed = computed(() => {
+    return product.value.price / 100
+})
+
+const oldPriceComputed = computed(() => {
+    let res = (product.value.price + (product.value.price / 10)) / 100
+    return res.toFixed(2)
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -12,4 +12,13 @@
 
 <script setup lang="ts">
 import MainLayout from "~/layouts/MainLayout.vue";
+
+const products = [
+    { id: 1, title: "title 1", description: "desc", url: "https://m.media-amazon.com/images/I/81IRC5sTOjL._AC_UF894,1000_QL80_.jpg", price: 1000 },
+    { id: 2, title: "title 2", description: "desc", url: "https://cdn1.coppel.com/images/catalog/pm/4303953-1.jpg", price: 1000 },
+    { id: 3, title: "title 3", description: "desc", url: "https://cdn1.coppel.com/images/catalog/pm/4304083-1.jpg", price: 1000 },
+    { id: 4, title: "title 4", description: "desc", url: "https://www.muebleslluesma.com/blog/wp-content/uploads/2021/06/banco-alfresco-loll-designs-960x960.jpg", price: 1000 },
+    { id: 5, title: "title 5", description: "desc", url: "https://www.muebleslluesma.com/blog/wp-content/uploads/2021/06/banco-adirondack-con-respaldo-loll-designs-960x960.jpg", price: 1000 },
+    { id: 6, title: "title 6", description: "desc", url: "https://misterwils.com/wp-content/uploads/2021/06/misterwils-blog-madera-reciclada-4.jpg", price: 1000 }
+]
 </script>
