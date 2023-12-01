93ce19fb1476e52d541f6748e14976a39a376e13
DESKTOP-EDFP8D1\Janus <godzillagmk2204@gmail.com>
2023-11-30 23:24:55 -0500

    modified:   components/CartItem.vue 	modified:   pages/shoppingcart.vue

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -9,7 +9,7 @@
                      class="flex items-center justify-center h-[20px] w-[20px] rounded-full border mr-5 ml-2"
                      :class="[isHover ? 'border-[#7841fa] border-[2px] transition-all' : 'border-gray-300', isSelected ? 'bg-[#7841fa]' : '']"
                 >
-                    <div class="h-[8px] w-[8px] rounded-full bg-white z-50"/>
+                    <div class="h-[8px] w-[8px] rounded-full bg-white"/>
                 </div>
             </div>
         </div>
@@ -17,8 +17,27 @@
         <div class="overflow-hidden pl-2 w-full">
             <div class="flex items-center justify-between w-full">
                 <div class="flex items-center justify-between truncate">
-                    <span class="sm:block hidden bg-[#7841fa] text-white text-[9px] font-semibold px-1.5 rounded-sm min-w-[80px]">Welcome deal</span>
+                    <span class="sm:block hidden bg-[#7841fa] bg-opacity-80 text-white text-[9px] font-bold px-1.5 py-0.5 rounded-sm w-auto">Oferta inicial</span>
+                    <div class="truncate sm:pl-2">
+                        {{ product.title }}
+                    </div>
                 </div>
+
+                <button @click="removeFromCart()"
+                        class="mx-2 w-9 sm:block hidden items-center justify-center p-1 pb-2 rounded-full bg-slate-600 text-white hover:bg-white hover:text-red-600 transition-all duration-300"
+                >
+                    <Icon name="material-symbols:delete-forever-outline-rounded" size="20"/>        
+                </button>
+            </div>
+            <div class="text-xl font-semibold">
+                $<span class="font-bold">{{ product.price / 100 }}</span>
+            </div>
+            <div class="flex items-center justify-end">
+                <button @click="removeFromCart()"
+                        class="w-9 sm:hidden block p-1 pb-2 rounded-full bg-slate-600 text-white hover:bg-white hover:text-red-600 transition-all duration-300"
+                >
+                    <Icon name="material-symbols:delete-forever-outline-rounded" size="20"/>        
+                </button>
             </div>
         </div>
     </div>
--- pages/shoppingcart.vue
+++ pages/shoppingcart.vue
@@ -30,6 +30,24 @@
                         </div>
                     </div>
                 </div>
+                <div class="md:hidden block my-4"/>
+
+                <div class="md:w-[35%]">
+                    <div id="Summary" class="bg-white rounded-lg p-4">
+                        <div class="text-2xl font-bold mb-2">Resumen</div>
+                        <div class="flex items-center justify-between my-4">
+                            <div class="font-semibold">Total</div>
+                            <div class="text-2xl font-semibold">
+                                <span class="font-bold">${{ totalPriceComputed }}</span>
+                            </div>
+                        </div>
+                        <button @click="goToCheckout"
+                                class="px-6 py-2 rounded-full w-full text-white text-lg font-semibold bg-gradient-to-tl from-[#fc66ff] via-[#954fff] to-[#cb53ff] bg-size-200 bg-pos-0 hover:bg-pos-100 transition-all duration-500 "
+                                >
+                            Pagar
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -38,9 +56,56 @@
 
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue';
+import { useUserStore } from '~/stores/user';
+
+const userStore = useUserStore()
+
+let selectedArray = ref([])
+
+onMounted(() => {
+    setTimeout(() => userStore.isLoading = false, 200)
+})
 
 const products = [
-    { id: 1, title: "Sillón gris de sala", description: "desc", url: "https://m.media-amazon.com/images/I/81IRC5sTOjL._AC_UF894,1000_QL80_.jpg", price: 1000 },
-    { id: 2, title: "Sofá gris de sala", description: "desc", url: "https://cdn1.coppel.com/images/catalog/pm/4303953-1.jpg", price: 1000 }
+    { id: 1, title: "Sillón gris de sala", description: "desc", url: "https://m.media-amazon.com/images/I/81IRC5sTOjL._AC_UF894,1000_QL80_.jpg", price: 98898 },
+    { id: 2, title: "Sofá gris de sala", description: "desc", url: "https://cdn1.coppel.com/images/catalog/pm/4303953-1.jpg", price: 197744 }
 ]
+
+const totalPriceComputed = computed(() => {
+    let price = 0
+    userStore.cart.forEach(prod => {
+        price += prod.price
+    })
+    return price / 100
+})
+
+const selectedRadioFunc = (e) => {
+    if(!selectedArray.value.length){
+        selectedArray.value.push(e)
+        return
+    }
+
+    selectedArray.value.forEach((item, index) => {
+        if(e.id != item.id){
+            selectedArray.value.push(e)
+        }
+        else{
+            selectedArray.value.splice(index, 1)
+        }
+    })
+}
+
+const goToCheckout = () => {
+    let ids = []
+    userStore.checkout = []
+    selectedArray.value.forEach(item => ids.push(item.id));
+
+    let res = userStore.cart.filter((item) => {
+        return ids.indexOf(item.id) != -1
+    })
+
+    res.forEach(item => userStore.checkout.push(toRaw(item)))
+
+    return navigateTo('/checkout')
+}
 </script>
